fbb327ab56e7c91d3e862e96818c87a298599cc6
Ali Alladin <ali.alladin20@gmail.com>
2020-11-02 19:40:58 +0100

    Completed 1. b)

--- week1/src/main.java
+++ week1/src/main.java
@@ -5,16 +5,24 @@ public class main {
 
     private void program() {
         System.out.println(isPalindrome("madamimadam"));
+        System.out.println(isPalindrome("Madam, I'm Adam!"));
     }
 
     boolean isPalindrome(String original){
+        String originalCopy = "";
         String reversed = "";
 
-        for (int i = original.length() - 1; i >= 0; i--) {
-            reversed += original.charAt(i);
+        for (int i = 0; i < original.length(); i++) {
+            if (Character.isAlphabetic(original.charAt(i))) {
+                originalCopy += Character.toUpperCase(original.charAt(i));
+            }
         }
 
-        if (original.equals(reversed)){
+        for (int i = originalCopy.length() - 1; i >= 0; i--) {
+            reversed += Character.toUpperCase(originalCopy.charAt(i));
+        }
+
+        if (originalCopy.equals(reversed)){
             return true;
         }
         else {
