397d1ab57506e6055c223aea8161967323948431
Ali Alladin <ali.alladin20@gmail.com>
2020-11-03 15:34:59 +0100

    Completed 2

--- week1/src/main.java
+++ week1/src/main.java
@@ -6,9 +6,35 @@ public class main {
     private void program() {
         System.out.println(isPalindrome("madamimadam"));
         System.out.println(isPalindrome("Madam, I'm Adam!"));
+
+        DynamicArray<Character> dynamicArray = new DynamicArray<Character>();
+
+        dynamicArray.add('A');
+        dynamicArray.add('B');
+        dynamicArray.add('C');
+
+        System.out.println(dynamicArray.get(0).equals('A'));
+        System.out.println(dynamicArray.get(1).equals('B'));
+        System.out.println(!dynamicArray.get(2).equals('B'));
+
+        dynamicArray.set(2, 'B');
+        System.out.println(dynamicArray.get(2).equals('B'));
+
+        try {
+            System.out.println(dynamicArray.get(4));
+        } catch (Exception exception) {
+            System.out.println(exception.toString());
+        }
+
+        try {
+            dynamicArray.set(-1, 'D');
+        } catch (Exception exception) {
+            System.out.println(exception.toString());
+        }
+
     }
 
-    boolean isPalindrome(String original){
+    boolean isPalindrome(String original) {
         String originalCopy = "";
         String reversed = "";
 
@@ -22,11 +48,46 @@ public class main {
             reversed += Character.toUpperCase(originalCopy.charAt(i));
         }
 
-        if (originalCopy.equals(reversed)){
+        if (originalCopy.equals(reversed)) {
             return true;
-        }
-        else {
+        } else {
             return false;
         }
     }
+
+
+    public static class DynamicArray<A> {
+        private Object[] array = new Object[1];
+        private int used = 0;
+
+        void add(A value) {
+            if (array.length == used) {
+                Object[] newArray = new Object[array.length * 2];
+                for (int i = 0; i < array.length; i++) {
+                    newArray[i] = array[i];
+                }
+                array = newArray;
+            }
+
+            array[used] = value;
+            used++;
+        }
+
+        A get(int index) {
+            if (index < 0 || index >= array.length) {
+                throw new ArrayIndexOutOfBoundsException();
+            }
+            return (A) array[index];
+        }
+
+        void set(int index, A value) {
+            if (index < 0 || index >= array.length) {
+                throw new ArrayIndexOutOfBoundsException();
+            }
+
+            array[index] = value;
+        }
+
+    }
 }
+
